0d84767ce5dcc05aa86c36b1e53ccaee237a0c16
andresrodriguez55 <57767201+andresrodriguez55@users.noreply.github.com>
2021-09-09 05:22:30 +0300

    Updates

--- Tools/InformationOfAlgorithms&Presentation/InformationGCD&ExtendedGCD.js
+++
@@ -1,8 +0,0 @@
-const information={
-    name: "GCD & Extended GCD",
-    information: "ff",
-    imageFileName1: "Gcd.jpg",
-    imageFileName2: "ExtendedGcd.jpg",
-    imageFileName3: "ExtendedGcdCode.png",
-};
-export default information;
--- Tools/Subcategories Names/Graphs.js
+++
@@ -1,2 +0,0 @@
-const information=["Deep-First Searh", "Breadth-First Search", "Shortest Path (Dijkstra)", "Minimum Spanning Tree (Kruskal)"];
-export default information;
